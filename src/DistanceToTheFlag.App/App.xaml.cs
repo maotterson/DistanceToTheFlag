5e261b12220714f4b176fd6af8be591c90ee8fc7
maotterson <otterson.2@gmail.com>
2022-06-13 15:16:08 -0400

    add maui blazor project

---
+++ src/DistanceToTheFlag.App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DistanceToTheFlag.App;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ src/DistanceToTheFlag.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DistanceToTheFlag.App;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
