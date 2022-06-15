5492cc1ba46219dc6bf4889b896f2f29f5e6ac1c
maotterson <otterson.2@gmail.com>
2022-06-15 00:28:27 -0400

    flesh out photograph distance calculation parameters

--- samples/DistanceToTheFlag.ImageCalculation/Program.cs
+++ samples/DistanceToTheFlag.ImageCalculation/Program.cs
@@ -10,16 +10,27 @@ var imageSource = string.Format("data:image/jpg;base64,{0}", base64image);
 var image = new Bitmap(photoPath);
 var items = image.PropertyItems;
 
+var photograph = new PhotographOfObject
+{
+    FocalLength = focalLength,
+    RealHeight = actualSubjectHeight,
+    ImageHeight = 1,
+    ObjectHeight = 1,
+    SensorHeight = sensorHeight
+};
+
+var distance = photograph.GetDistanceToObject();
+
 internal class PhotographOfObject
 {
-    public double F { get; init; }
+    public double FocalLength { get; init; }
     public double RealHeight { get; init; }
     public int ImageHeight { get; init; } // pixels
     public int ObjectHeight { get; init; } // pixels
     public double SensorHeight { get; init; }
     public double GetDistanceToObject()
     {
-        return (F * RealHeight * ImageHeight) /
+        return (FocalLength * RealHeight * ImageHeight) /
             (ObjectHeight * SensorHeight);
     }
 
